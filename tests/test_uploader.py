12555e88f9fe66062b900795f7083b4c0b254ea8
Roy Willy Haug <ROWH@equinor.com>
2023-10-02 14:56:39 +0200

    Print to debug

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -292,10 +292,12 @@ def test_openvds_available():
     python_path = os.path.dirname(sys.executable)
     logger.info(python_path)
     path_to_SEGYImport = os.path.join(python_path, '..', 'bin', 'SEGYImport')
+    print("Path to segyimport, 1: " + path_to_SEGYImport)
     if sys.platform.startswith("win"):
         path_to_SEGYImport = path_to_SEGYImport + ".exe"
     if not os.path.isfile(path_to_SEGYImport):
         path_to_SEGYImport = os.path.join(python_path, '..', 'shims', 'SEGYImport')
+    print("Path to segyimport, 2: " + path_to_SEGYImport)
 
     logger.info(path_to_SEGYImport)
     check_SEGYImport_version = subprocess.run([path_to_SEGYImport, '--version'], 
