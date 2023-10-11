ca85635a95c50a93e2991ae009485ac84a84cc59
Raymond Wiker <rwiker@gmail.com>
2023-10-11 12:26:24 +0200

    Refactor, and let sumo-wrapper handle retries. (#14)
    * Rewrite/refactor to use new SumoClient implementation (including the automatic retries).
    * mobbing: fixes, improvements and refactoring
    
    ---------
    
    Co-authored-by: Raymond Wiker <rayw@equinor.com>
    Co-authored-by: Runar Ask Johannessen <ruaj@equinor.com>

--- src/fmu/sumo/uploader/_fileonjob.py
+++ src/fmu/sumo/uploader/_fileonjob.py
@@ -6,38 +6,18 @@
 
 """
 
-import os
-import datetime
-import time
 import logging
 import hashlib
 import base64
-import tempfile
-import json
 
-import yaml
-
-from sumo.wrapper._request_error import (
-    AuthenticationError,
-    TransientError,
-    PermanentError,
-)
-
-from azure.core.exceptions import ResourceExistsError
+from fmu.sumo.uploader._sumofile import SumoFile
 
 # pylint: disable=C0103 # allow non-snake case variable names
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.CRITICAL)
 
-def parse_yaml(path):
-    """From path, parse file as yaml, return data"""
-    with open(path, "r") as stream:
-        data = yaml.safe_load(stream)
-
-    return data
-
-class FileOnJob:
+class FileOnJob(SumoFile):
     def __init__(self, byte_string: str, metadata):
         """
         path (str): Path to file
@@ -63,146 +43,3 @@ class FileOnJob:
         self.metadata["file"]["absolute_path"] = ""
         self.metadata["file"]["checksum_md5"] = self.metadata["_sumo"]["blob_md5"]
 
-    def _upload_metadata(self, sumo_connection, sumo_parent_id):
-        path = f"/objects('{sumo_parent_id}')"
-        response = sumo_connection.api.post(path=path, json=self.metadata)
-        return response
-
-    def _upload_byte_string(self, sumo_connection, object_id, blob_url):
-        response = sumo_connection.api.blob_client.upload_blob(blob=self.byte_string, url=blob_url)
-        return response
-
-    def _delete_metadata(self, sumo_connection, object_id):
-        path = f"/objects('{object_id}')"
-        response = sumo_connection.api.delete(path=path)
-        return response
-
-    def upload_to_sumo(self, sumo_parent_id, sumo_connection):
-        """Upload this file to Sumo"""
-
-        logger.debug("Starting upload_to_sumo()")
-
-        if not sumo_parent_id:
-            raise ValueError(
-                f"Upload failed, sumo_parent_id passed to upload_to_sumo: {sumo_parent_id}"
-            )
-
-        _t0 = time.perf_counter()
-        _t0_metadata = time.perf_counter()
-
-        result = {}
-
-        backoff = [1, 3, 9]
-
-        for i in backoff:
-            logger.debug("backoff in outer loop is %s", str(i))
-
-            try:
-                response = self._upload_metadata(
-                    sumo_connection=sumo_connection, sumo_parent_id=sumo_parent_id
-                )
-
-                _t1_metadata = time.perf_counter()
-
-                result["metadata_upload_response_status_code"] = response.status_code
-                result["metadata_upload_response_text"] = response.text
-                result["metadata_upload_time_start"] = _t0_metadata
-                result["metadata_upload_time_end"] = _t1_metadata
-                result["metadata_upload_time_elapsed"] = _t1_metadata - _t0_metadata
-
-            except TransientError as err:
-                logger.debug("TransientError on blob upload. Sleeping %s", str(i))
-                result["status"] = "failed"
-                result["metadata_upload_response_status_code"] = err.code
-                result["metadata_upload_response_text"] = err.message
-                time.sleep(i)
-                continue
-
-            except AuthenticationError as err:
-                result["status"] = "rejected"
-                result["metadata_upload_response_status_code"] = err.code
-                result["metadata_upload_response_text"] = err.message
-                return result
-            except PermanentError as err:
-                result["status"] = "rejected"
-                result["metadata_upload_response_status_code"] = err.code
-                result["metadata_upload_response_text"] = err.message
-                return result
-
-            break
-
-        if result["metadata_upload_response_status_code"] not in [200, 201]:
-            return result
-
-        self.sumo_parent_id = sumo_parent_id
-        self.sumo_object_id = response.json().get("objectid")
-
-        blob_url = response.json().get("blob_url")
-
-        # UPLOAD BLOB
-
-        _t0_blob = time.perf_counter()
-        upload_response = {}
-        for i in backoff:
-            logger.debug("backoff in inner loop is %s", str(i))
-            try:
-                response = self._upload_byte_string(
-                    sumo_connection=sumo_connection,
-                    object_id=self.sumo_object_id,
-                    blob_url=blob_url,
-                )
-                upload_response["status_code"] = response.status_code
-                upload_response["text"] = response.text
-
-                _t1_blob = time.perf_counter()
-
-                result["blob_upload_response_status_code"] = upload_response[
-                    "status_code"
-                ]
-                result["blob_upload_response_status_text"] = upload_response["text"]
-                result["blob_upload_time_start"] = _t0_blob
-                result["blob_upload_time_end"] = _t1_blob
-                result["blob_upload_time_elapsed"] = _t1_blob - _t0_blob
-            except ResourceExistsError as err:
-                upload_response["status_code"] = 200
-                upload_response["text"] = "File hopefully uploaded to Oneseimic"
-                _t1_blob = time.perf_counter()
-
-                result["blob_upload_response_status_code"] = upload_response[
-                    "status_code"
-                ]
-                result["blob_upload_response_status_text"] = upload_response["text"]
-                result["blob_upload_time_start"] = _t0_blob
-                result["blob_upload_time_end"] = _t1_blob
-                result["blob_upload_time_elapsed"] = _t1_blob - _t0_blob
-
-            except OSError as err:
-                logger.debug("Upload failed: %s", str(err))
-                result["status"] = "failed"
-                self._delete_metadata(sumo_connection, self.sumo_object_id)
-                return result
-            except TransientError as err:
-                logger.debug("Got TransientError. Sleeping for %i seconds", str(i))
-                result["status"] = "failed"
-                time.sleep(i)
-                continue
-            except AuthenticationError as err:
-                logger.debug("Upload failed: %s", upload_response["text"])
-                result["status"] = "rejected"
-                self._delete_metadata(sumo_connection, self.sumo_object_id)
-                return result
-            except PermanentError as err:
-                logger.debug("Upload failed: %s", upload_response["text"])
-                result["status"] = "rejected"
-                self._delete_metadata(sumo_connection, self.sumo_object_id)
-                return result
-
-            break
-
-        if upload_response["status_code"] not in [200, 201]:
-            logger.debug("Upload failed: %s", upload_response["text"])
-            result["status"] = "failed"
-            self._delete_metadata(sumo_connection, self.sumo_object_id)
-        else:
-            result["status"] = "ok"
-        return result
---
+++ src/fmu/sumo/uploader/_sumofile.py
@@ -0,0 +1,268 @@
+"""
+
+Base class for FileOnJob and FileOnDisk classes.
+
+"""
+
+
+import os
+import sys
+import time
+import subprocess
+import logging
+import httpx
+
+# pylint: disable=C0103 # allow non-snake case variable names
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.CRITICAL)
+
+
+def _get_segyimport_cmdstr(blob_url, object_id, file_path, sample_unit):
+    """Return the command string for running OpenVDS SEGYImport"""
+    try:
+        url = "azureSAS:" + blob_url["baseuri"][6:]
+        url_conn = "Suffix=?" + blob_url["auth"]
+    except KeyError:
+        url = (
+            "azureSAS" + blob_url.split(object_id)[0][5:]
+        )  # SEGYImport expects url to container
+        url_conn = "Suffix=?" + blob_url.split("?")[1]
+
+    persistent_id = object_id
+
+    segy_command = "SEGYImport"
+    if sys.platform.startswith("win"):
+        segy_command = segy_command + ".exe"
+    python_path = os.path.dirname(sys.executable)
+    # The SEGYImport folder location is not fixed
+    locations = [
+        os.path.join(python_path, "bin"),
+        os.path.join(python_path, "..", "bin"),
+        os.path.join(python_path, "..", "shims"),
+        "/home/vscode/.local/bin",
+        "/usr/local/bin",
+    ]
+    path_to_executable = None
+    for loc in locations:
+        path = os.path.join(loc, segy_command)
+        if os.path.isfile(path):
+            path_to_executable = path
+            break
+    if path_to_executable is None:
+        logger.error("Could not find OpenVDS executables folder location")
+    logger.info("Path to OpenVDS executable: " + path_to_executable)
+
+    cmdstr = [
+        path_to_executable,
+        "--compression-method",
+        "RLE",
+        "--brick-size",
+        "64",
+        "--sample-unit",
+        sample_unit,
+        "--url",
+        url,
+        "--url-connection",
+        url_conn,
+        "--persistentID",
+        persistent_id,
+        file_path,
+    ]
+
+    return cmdstr
+
+
+class SumoFile:
+    def __init__(self):
+        return
+
+    def _upload_metadata(self, sumo_connection, sumo_parent_id):
+        path = f"/objects('{sumo_parent_id}')"
+        response = sumo_connection.api.post(path=path, json=self.metadata)
+        return response
+
+    def _upload_byte_string(self, sumo_connection, object_id, blob_url):
+        response = sumo_connection.api.blob_client.upload_blob(
+            blob=self.byte_string, url=blob_url
+        )
+        return response
+
+    def _delete_metadata(self, sumo_connection, object_id):
+        path = f"/objects('{object_id}')"
+        response = sumo_connection.api.delete(path=path)
+        return response
+
+    def upload_to_sumo(self, sumo_parent_id, sumo_connection):
+        """Upload this file to Sumo"""
+
+        logger.debug("Starting upload_to_sumo()")
+
+        if not sumo_parent_id:
+            raise ValueError(
+                f"Upload failed, missing sumo_parent_id. Got: {sumo_parent_id}"
+            )
+
+        _t0 = time.perf_counter()
+        _t0_metadata = time.perf_counter()
+
+        # We need these included even if returning before blob upload
+        result = {"blob_file_path": self.path, "blob_file_size": self._size}
+
+        # Uploader converts segy-files to OpenVDS:
+        if self.metadata["data"]["format"] in ["openvds", "segy"]:
+            self.metadata["data"]["format"] = "openvds"
+            self.metadata["file"]["checksum_md5"] = ""
+
+        try:
+            response = self._upload_metadata(
+                sumo_connection=sumo_connection, sumo_parent_id=sumo_parent_id
+            )
+
+            _t1_metadata = time.perf_counter()
+
+            result.update(
+                {
+                    "metadata_upload_response_status_code": response.status_code,
+                    "metadata_upload_response_text": response.text,
+                    "metadata_upload_time_start": _t0_metadata,
+                    "metadata_upload_time_end": _t1_metadata,
+                    "metadata_upload_time_elapsed": _t1_metadata - _t0_metadata,
+                    "metadata_file_path": self.metadata_path,
+                    "metadata_file_size": self._size,
+                }
+            )
+            pass
+        except (httpx.TimeoutException, httpx.ConnectError) as err:
+            result.update(
+                {
+                    "status": "failed",
+                    "metadata_upload_response_status_code": 500,
+                    "metadata_upload_response_text": str(err),
+                }
+            )
+            pass
+        except httpx.HTTPStatusError as err:
+            result.update(
+                {
+                    "status": "failed",
+                    "metadata_upload_response_status_code": err.response.status_code,
+                    "metadata_upload_response_text": err.response.reason_phrase,
+                }
+            )
+            pass
+
+        if result["metadata_upload_response_status_code"] not in [200, 201]:
+            return result
+
+        self.sumo_parent_id = sumo_parent_id
+        self.sumo_object_id = response.json().get("objectid")
+
+        blob_url = response.json().get("blob_url")
+
+        # UPLOAD BLOB
+
+        _t0_blob = time.perf_counter()
+        upload_response = {}
+
+        if self.metadata["data"]["format"] in ["openvds", "segy"]:
+            if sys.platform.startswith("darwin"):
+                # OpenVDS does not support Mac/darwin directly
+                # Outer code expects and interprets http error codes
+                upload_response.update(
+                    {
+                        "status_code": 418,
+                        "text": "Can not perform SEGY upload since OpenVDS does not support Mac",
+                    }
+                )
+            else:
+                if self.metadata["data"]["vertical_domain"] == "depth":
+                    sample_unit = "m"
+                else:
+                    sample_unit = "ms"  # aka time domain
+
+                cmd_str = _get_segyimport_cmdstr(
+                    blob_url, self.sumo_object_id, self.path, sample_unit
+                )
+                try:
+                    cmd_result = subprocess.run(
+                        cmd_str, capture_output=True, text=True, shell=False
+                    )
+                    if cmd_result.returncode == 0:
+                        upload_response.update(
+                            {
+                                "status_code": 200,
+                                "text": "SEGY uploaded as OpenVDS.",
+                            }
+                        )
+                    else:
+                        # Outer code expects and interprets http error codes
+                        upload_response.update(
+                            {
+                                "status_code": 418,
+                                "text": "FAILED SEGY upload as OpenVDS. "
+                                + cmd_result.stderr,
+                            }
+                        )
+                        pass
+                    pass
+
+                except OSError as err:
+                    upload_response.update(
+                        {
+                            "status_code": 418,
+                            "text": "FAILED SEGY upload as OpenVDS. " + str(err),
+                        }
+                    )
+        else:
+            try:
+                response = self._upload_byte_string(
+                    sumo_connection=sumo_connection,
+                    object_id=self.sumo_object_id,
+                    blob_url=blob_url,
+                )
+                upload_response.update(
+                    {"status_code": response.status_code, "text": response.text}
+                )
+                pass
+            except (httpx.TimeoutException, httpx.ConnectError) as err:
+                upload_response.update(
+                    {
+                        "status": "failed",
+                        "blob_upload_response_status_code": 500,
+                        "blob_upload_response_text": str(err),
+                    }
+                )
+                pass
+            except httpx.HTTPStatusError as err:
+                upload_response.update(
+                    {
+                        "status": "failed",
+                        "blob_upload_response_status_code": err.response.status_code,
+                        "blob_upload_response_text": err.response.reason_phrase,
+                    }
+                )
+                pass
+
+        _t1_blob = time.perf_counter()
+
+        result.update(
+            {
+                "blob_upload_response_status_code": upload_response["status_code"],
+                "blob_upload_response_status_text": upload_response["text"],
+                "blob_upload_time_start": _t0_blob,
+                "blob_upload_time_end": _t1_blob,
+                "blob_upload_time_elapsed": _t1_blob - _t0_blob,
+            }
+        )
+
+        if "status_code" not in upload_response or upload_response[
+            "status_code"
+        ] not in [200, 201]:
+            logger.debug("Upload failed: %s", upload_response["text"])
+            result["status"] = "failed"
+            self._delete_metadata(sumo_connection, self.sumo_object_id)
+        else:
+            result["status"] = "ok"
+
+        return result
