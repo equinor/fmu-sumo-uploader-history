eac9755209c0c0781c1efa7316341869363a5769
Runar Ask Johannessen <ruaj@equinor.com>
2023-11-20 15:10:52 +0100

    move ert hooks and jobs to uploader folder

--- src/fmu/sumo/uploader/hook_implementations/jobs.py
+++ src/fmu/sumo/uploader/hook_implementations/jobs.py
@@ -21,9 +21,11 @@ def _get_jobs_from_directory(directory):
 
 # pylint: disable=no-value-for-parameter
 @hook_implementation
-@plugin_response(plugin_name="fmu_sumo")  # pylint: disable=no-value-for-parameter
+@plugin_response(
+    plugin_name="fmu_sumo"
+)  # pylint: disable=no-value-for-parameter
 def installable_jobs():
-    return _get_jobs_from_directory("sumo/config_jobs")
+    return _get_jobs_from_directory("sumo/uploader/config_jobs")
 
 
 def _get_module_variable_if_exists(module_name, variable_name, default=""):
@@ -36,7 +38,9 @@ def _get_module_variable_if_exists(module_name, variable_name, default=""):
 
 
 @hook_implementation
-@plugin_response(plugin_name="fmu_sumo")  # pylint: disable=no-value-for-parameter
+@plugin_response(
+    plugin_name="fmu_sumo"
+)  # pylint: disable=no-value-for-parameter
 def job_documentation(job_name):
     sumo_fmu_jobs = set(installable_jobs().data.keys())
     if job_name not in sumo_fmu_jobs:
